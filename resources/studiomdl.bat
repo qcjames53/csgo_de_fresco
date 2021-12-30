c89c09f80560749e0a766566c4713722ef2a7d58
qcjames53 <qj@quinnjam.es>
2021-12-29 21:40:43 -0500

    Initial commit with current working progress

---
+++ resources/studiomdl.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Steam\SteamApps\common\Counter-Strike Global Offensive\bin\studiomdl" -game "C:\Program Files (x86)\Steam\SteamApps\common\Counter-Strike Global Offensive\csgo" %1
+pause
---
+++ resources/textures_com/converted/remove_vmt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for file in *.vmt; do
+	rm $file
+done;
